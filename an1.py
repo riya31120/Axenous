08746cda3510274949627122905a58906ac3c92e
riya31120 <71517489+riya31120@users.noreply.github.com>
2021-02-08 10:53:13 +0530

    Add files via upload

---
+++ an1.py
@@ -0,0 +1,28 @@
+text = 'BOOKKEEPER'
+new = list(text)
+x=10
+def countX(new,x):
+	count=0
+	for ele in lst: 
+		if (ele == x): 
+			count = count + 1
+			return count 
+def listToString(new):  
+    
+     
+    str1 = ""  
+    
+       
+    for ele in s:  
+        str1 += ele   
+    BOOKKEEPER
+    
+    return str1  
+        
+if countX==2:
+	new.replace("O","Z")  
+	new.replace("O","K")
+if countX > 2:
+	new.replace("E","X")
+     
+print(new)
